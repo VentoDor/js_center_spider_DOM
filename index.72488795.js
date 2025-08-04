d246bc967afc5347f70e529ffc2bf2bcbabcf0c8
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-04 19:54:25 +0300

    make build

---
+++ index.72488795.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,i=e.clientHeight,c=t.clientWidth,n=t.clientHeight;t.style.left=l/2-c/2+"px",t.style.top=i/2-n/2+"px";
+//# sourceMappingURL=index.72488795.js.map
